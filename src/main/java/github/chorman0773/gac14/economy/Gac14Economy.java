7c7b12c04ff51f6b539f2c3aac44127029bdbd0c
Connor Horman <chorman64@gmail.com>
2019-05-12 14:31:24 -0400

    Fix modid

--- src/main/java/github/chorman0773/gac14/economy/Gac14Economy.java
+++ src/main/java/github/chorman0773/gac14/economy/Gac14Economy.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("gac14-economy")
 public class Gac14Economy
 {
     // Directly reference a log4j logger.
